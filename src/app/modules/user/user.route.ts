6d794a8af345c24bd10d7a1f4d09ad9690ba904d
Romjan Ali <000romjanali@gmail.com>
2025-07-25 17:41:47 +0600

    part 5

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,11 +4,30 @@ import { validateRequest } from '../../middlewares/validateRequest'
 import { createUserZodSchema, updateUserZodSchema } from './user.validation'
 import { checkAuth } from '../../middlewares/checkAuth'
 import { Role } from './user.interface'
+import { DivisionController } from '../division/division.controller'
 
 const router = Router()
 
-router.post('/register', validateRequest(createUserZodSchema), UserControllers.createUser)
-router.get('/all-users', checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUser)
-router.patch('/:id', validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser)
+router.post(
+  '/register',
+  validateRequest(createUserZodSchema),
+  UserControllers.createUser
+)
+router.get(
+  '/all-users',
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  UserControllers.getAllUser
+)
+router.patch(
+  '/:id',
+  validateRequest(updateUserZodSchema),
+  checkAuth(...Object.values(Role)),
+  UserControllers.updateUser
+)
+router.delete(
+  '/:id',
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  DivisionController.deleteDivision
+)
 
 export const UserRoutes = router
